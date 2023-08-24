b0177e90557305b3b42abc9150448c26b33551b7
AzureAD\BartlomiejProce <barlomiej.proce@macrix.pl>
2023-08-24 13:12:13 +0200

    Frist try

---
+++ Hotel/Models/ReservationBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservoom.Exceptions;
+
+namespace Hotel.Models
+{
+    class ReservationBook
+    {
+        private readonly List<Reservation> _roomsReservations;
+
+        public ReservationBook() {
+
+            _roomsReservations = new  List<Reservation>();
+        }
+
+        public IEnumerable<Reservation> GetAllReservations() { 
+        return _roomsReservations;
+        }
+
+        public void AddReservation(Reservation reservation) {
+            foreach (Reservation existingReservation in _roomsReservations)
+            {
+                if (existingReservation.Conflicts(reservation))
+                {
+                    throw new ReservationConflictException(existingReservation, reservation);
+                }
+            }
+            _roomsReservations.Add(reservation);
+        }
+    }
+}
---
+++ Hotel/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Models;
+
+namespace Hotel.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        private readonly Reservation _reservation;
+
+        public string RoomID => _reservation.RoomID.ToString();
+        public string Username => _reservation.Username;
+        public DateTime StartTime => _reservation.StartTime;
+        public DateTime EndTime => _reservation.EndTime;
+        public ReservationViewModel(Reservation reservation)
+        {
+            _reservation = reservation;
+        }
+    }
+}
